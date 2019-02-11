dc348bb62d37a33aa83824843c2636249c7afb1d
David Khourshid <davidkpiano@gmail.com>
2019-02-11 13:49:58 -0500

    Update david-khourshid.ts

--- content/react-finland-2019/talks/david-khourshid.ts
+++ content/react-finland-2019/talks/david-khourshid.ts
@@ -4,8 +4,8 @@ import speaker from "../../people/david-khourshid";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious talk",
-  description: ``,
+  title: "Mind-Reading with Adaptive and Intelligent User Interfaces",
+  description: `What if you could predict user behavior with smart UIs? In this talk, we will explore how we can make adaptive and intelligent user interfaces in React that learn from how individual users use your apps, and personalize the interface and features just for them, in real-time. With probability-driven statecharts, decision trees, reinforcement learning and more, UIs can be developed in such a way that it automatically adapts to the user's behavior.`,
   type: SessionType.TALK,
   keywords: [],
 };
